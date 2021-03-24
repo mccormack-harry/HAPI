012220d95730b6b71db45a9fa021440b964ef2b7
haz8989 <mccormack_harry@hotmail.com>
2021-03-24 14:49:35 +0000

    Update EffectEnchantment Processing

--- src/main/java/me/hazedev/hapi/enchanting/EffectEnchantment.java
+++ src/main/java/me/hazedev/hapi/enchanting/EffectEnchantment.java
@@ -32,27 +32,37 @@ public class EffectEnchantment extends CustomEnchantment implements Listener {
     @EventHandler
     public void onPlayerSwitchItem(PlayerItemHeldEvent event){
         Player player = event.getPlayer();
-        int level = getManager().getEnchantmentLevel(player.getInventory().getItem(event.getNewSlot()), this);
-        updateEffect(player, level);
+        Bukkit.getScheduler().runTask(manager.getComponent().getPlugin(), () -> updateEffect(player));
     }
 
     @EventHandler
     public void onEnchantEvent(EnchantEvent event) {
         if (event.getEnchantment() == this) {
             Player player = event.getPlayer();
-            int level = event.getCurrentLevel();
-            updateEffect(player, level);
+            Bukkit.getScheduler().runTask(manager.getComponent().getPlugin(), () -> updateEffect(player));
         }
-
     }
 
-    protected void updateEffect(Player player, int level) {
+    protected void removeEffect(Player player) {
+        if (activated.contains(player.getUniqueId())) {
+            player.removePotionEffect(potionEffectType);
+            activated.remove(player.getUniqueId());
+        }
+    }
+
+    protected void addEffect(Player player, int level) {
         if (level > 0) {
             player.addPotionEffect(new PotionEffect(potionEffectType, Integer.MAX_VALUE, level - 1, false, false, true));
             activated.add(player.getUniqueId());
-        } else if (activated.contains(player.getUniqueId())) {
-            player.removePotionEffect(potionEffectType);
-            activated.remove(player.getUniqueId());
+        }
+    }
+
+    protected void updateEffect(Player player) {
+        int level = getManager().getEnchantmentLevel(player, this);
+        if (level > 0) {
+            addEffect(player, level);
+        } else {
+            removeEffect(player);
         }
     }
 
