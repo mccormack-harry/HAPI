704452168d22e6dc962b07e88040e259f7209485
haz8989 <mccormack_harry@hotmail.com>
2021-04-07 03:26:32 +0100

    Add more constructors for CommandDelegator

--- src/main/java/me/hazedev/hapi/command/CommandDelegator.java
+++ src/main/java/me/hazedev/hapi/command/CommandDelegator.java
@@ -1,8 +1,6 @@
 package me.hazedev.hapi.command;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
+import org.bukkit.command.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,22 +9,36 @@ import java.util.List;
 
 public class CommandDelegator extends Command {
 
-    private final TabExecutor tabExecutor;
+    private final CommandExecutor commandExecutor;
+    private final TabCompleter tabCompleter;
 
     public CommandDelegator(@NotNull TabExecutor tabExecutor, @NotNull String name, @NotNull String description, @NotNull String usageMessage, @Nullable List<String> aliases) {
+        this(tabExecutor, tabExecutor, name, description, usageMessage, aliases);
+    }
+
+    public CommandDelegator(@Nullable TabCompleter tabCompleter, @NotNull String name, @NotNull String description, @NotNull String usageMessage, @Nullable List<String> aliases) {
+        this(null, tabCompleter, name, description, usageMessage, aliases);
+    }
+
+    public CommandDelegator(@Nullable CommandExecutor commandExecutor, @NotNull String name, @NotNull String description, @NotNull String usageMessage, @Nullable List<String> aliases) {
+        this(commandExecutor, null, name, description, usageMessage, aliases);
+    }
+
+    public CommandDelegator(@Nullable CommandExecutor commandExecutor, @Nullable TabCompleter tabCompleter, @NotNull String name, @NotNull String description, @NotNull String usageMessage, @Nullable List<String> aliases) {
         super(name, description, usageMessage, aliases != null ? aliases : Collections.emptyList());
-        this.tabExecutor = tabExecutor;
+        this.commandExecutor = commandExecutor;
+        this.tabCompleter = tabCompleter;
     }
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) {
-        return tabExecutor.onCommand(sender, this, alias, args);
+        return commandExecutor.onCommand(sender, this, alias, args);
     }
 
     @NotNull
     @Override
     public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
-        List<String> result = tabExecutor.onTabComplete(sender, this, alias, args);
+        List<String> result = tabCompleter.onTabComplete(sender, this, alias, args);
         return result != null ? result : Collections.emptyList();
     }
 }
